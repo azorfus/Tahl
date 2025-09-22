0e62ab1b502a90f41b2f085b7185af1ce82dad5e
azorfus <azorfus@gmail.com>
2025-09-22 15:46:50 +0530

    Refactoring and writing Task Scheduler

--- taskscheduler.hpp
+++ taskscheduler.hpp
@@ -47,7 +47,7 @@ public:
         }
 
         // Combine results from all threads
-        for(auto &child : root->children) {
+        for(auto &child : *root->children) {
             root->score += child->score;
             root->simulations += child->simulations;
         }
