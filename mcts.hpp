1bbc9bda965f9c04c952d07e71053572ab0a4939
azorfus <azorfus@gmail.com>
2025-09-22 15:10:50 +0530

    Refactoring and writing Task Scheduler

---
+++ mcts.hpp
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <thread>
+#include <unordered_map>
+#include "chess.hpp"
+
+
+class MCTSTree {
+public:
+    MCTSNode* root;
+
+    MCTSTree(chess::Board state) {
+        root = new MCTSNode(nullptr, state, chess::Move());
+    }
+
+    ~MCTSTree() {
+        delete root;   
+    }
+
+    void run_search(int iterations) {
+        for(int i = 0; i < iterations; i++) {
+            
+            MCTSNode* walker = root;
+
+            // Selection
+            while(!walker->children->empty() && walker->is_fully_expanded()) {
+                walker = walker->best_child();
+            }
+            
+            // Expansion
+            if(!walker->is_terminal()) {
+                walker = walker->expand();
+            
+                // Rollout
+                double result = walker->rollout();
+                walker->simulations++;
+                walker->score += result;
+            }
+        }
+    }
+}
+
+class MCTSNode {
+public:
+    bool terminal;
+    bool turn; // turn=true for WHITE, turn=false for BLACK
+    chess::Board state;
+    MCTSNode *parent;
+    chess::Move action;
+    std::vector<MCTSNode*> *children;
+    unsigned int simulations;
+    double score;
+    chess::Movelist untried_actions;
+
+    MCTSNode(MCTSNode *parent, chess::Board state, chess::Move action)
+        : parent(parent), state(state), action(action), score(0.0), simulations(0){
+            children = new std::vector<MCTSNode*>();
+            children->reserve(32);
+            actions_to_try(state);
+            terminal = is_terminal(state);
+            turn = state.sideToMove() == chess::Color::WHITE;
+        }
+
+    ~MCTSNode(){
+        for (auto* child : *children) delete child;
+        delete children;
+    }
+
+    void actions_to_try(chess::Board state){
+        return chess::movegen::legalmoves(untried_actions, state);
+    }
+
+    bool is_terminal(chess::Board state){
+        return state.isInsufficientMaterial() || state.isRepetition() || state.isHalfMoveDraw() || (state.getHalfMoveDrawType().first==chess::GameResultReason::CHECKMATE) || (state.getHalfMoveDrawType().first==chess::GameResultReason::STALEMATE);
+    }
+
+    int evaluate(chess::Board state) {
+        if (state.isInsufficientMaterial() || state.isRepetition() || state.isHalfMoveDraw()
+            || (state.getHalfMoveDrawType().first==chess::GameResultReason::STALEMATE)) {
+            return 0;
+        }
+        else if(state.getHalfMoveDrawType().first==chess::GameResultReason::CHECKMATE) {
+            if(state.sideToMove() == chess::Color::WHITE) return -1;
+            else return 1;
+        }
+        return 0;
+    }
+
+    MCTSNode* expand(){
+        chess::Move move = untried_actions.back();
+        chess::Movelist new_moves_list;
+
+        for (auto m : untried_actions){
+            if (m != move) new_moves_list.add(m);
+        }
+
+        untried_actions = new_moves_list;
+        chess::Board new_state = state;
+        new_state.makeMove(move);
+
+        MCTSNode* child = new MCTSNode(this, new_state, move);
+
+        children->push_back(child);
+
+        return child;
+    }
+
+    double rollout() {
+        chess::Board sim_state = state;
+
+        while(!is_terminal(sim_state)) {
+            chess::Movelist moves;
+            chess::movegen::legalmoves(moves, sim_state);
+
+            if(moves.empty()) break;
+
+            srand(time(0));
+            chess::Move random_move = moves[rand() % moves.size()];
+            sim_state.makeMove(random_move);
+        }
+
+        score = evaluate(sim_state);
+
+        return score;
+    }
+
+    bool is_fully_expanded() {
+        return terminal || untried_actions.empty();
+    }
+
+    MCTSNode* best_child(double c) {
+        if (children->empty()) return nullptr;
+
+        double ucb, m=-1;
+        MCTSNode* best = nullptr;
+
+        for (MCTSNode *child : *children){
+            double q = child->score / ((double) child->simulations);
+            ucb = c * sqrt((((double) this->simulations)) / ((double) child->simulations));
+
+            if (turn) ucb += q;
+            else ucb -= q;
+            
+            if (ucb > m){
+                m = ucb;
+                best = child;
+            }
+        }
+
+        return best;
+    } 
+
+    /* REDUNDANT FOR OUR CASE:
+
+    void backpropagate(double result) {
+        MCTSNode* node = this;
+
+        while(node) {
+            node->simulations++;
+            node->score += result;
+            node = node->parent;
+        }
+    } */
+};
---
+++ taskscheduler.hpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <thread>
+#include <unordered_map>
+#include "chess.hpp"
+#include "mcts.hpp"
+
+
+class TaskScheduler {
+
+public:
+    TaskScheduler(MCTSNode* root) {
+        this->root = root;
+    }
+    ~TaskScheduler() {}
+
+    MCTSNode* root;
+
+    void summon_threads(int threads) {
+        std::vector<std::thread> daemons;
+        std::vector<MCTSTree*> mcts_trees;
+
+        for(int t = 0; t < threads; t++) {
+            mcts_trees.push_back(new MCTSTree(root->state));
+            daemons.push_back(std::thread(&MCTSTree::run_search,
+                              mcts_trees.back(), 100 /*The number of iterations each thread*/ ));
+        }
+
+        for(int t = 0; t < threads; t++) {
+            daemons[t].join();
+        }
+
+        // Combine results from all threads
+        
+        
+    }
+}
