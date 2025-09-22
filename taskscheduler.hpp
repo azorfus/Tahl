e30da70aeb64564a4bc1cacd4a41613f4d17bd98
azorfus <azorfus@gmail.com>
2025-09-22 15:21:32 +0530

    Refactoring and writing Task Scheduler

--- taskscheduler.hpp
+++ taskscheduler.hpp
@@ -13,10 +13,10 @@ public:
 
     void summon_threads(int threads) {
         std::vector<std::thread> daemons;
-        std::vector<MCTSTree*> mcts_trees;
+        std::vector<MCTSTree> mcts_trees;
 
         for(int t = 0; t < threads; t++) {
-            mcts_trees.push_back(new MCTSTree(root->state));
+            mcts_trees.push_back(std::make_unique<MCTSTree>(root->state));
             daemons.push_back(std::thread(&MCTSTree::run_search,
                               mcts_trees.back(), 100 /*The number of iterations each thread*/ ));
         }
